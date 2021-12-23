bbd7ebc91165d6988150e78233d4988b63e2b7e4
Andrey Kudinov <2499931@gmail.com>
2021-12-24 00:21:52 +0400

    add forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import './slider';
-import { modals, tabs } from './modules/';
+import { modals, tabs, forms } from './modules/';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals()
   tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active')
   tabs('.decoration_slider','.no_click', '.decoration_content > div > div', 'after_click')
+  forms()
 })
---
+++ src/js/modules/forms.js
@@ -0,0 +1,60 @@
+export const forms = () => {
+  const forms = document.querySelectorAll('form'),
+    inputs = document.querySelectorAll('input'),
+    phoneInputs = document.querySelectorAll('input[name="user_phone"]')
+
+  phoneInputs.forEach(phoneInput => {
+    phoneInput.addEventListener('input', () => {
+      phoneInput.value = phoneInput.value.replace(/\D/, '')
+    })
+  })
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  }
+
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading
+
+    const options = {
+      method: 'POST',
+      body: data
+    }
+
+    const res = await fetch(url, options)
+
+    return await res.text()
+  }
+
+  const clearInputs = () => {
+    inputs.forEach(input => input.value = '')
+  }
+
+  forms.forEach(form => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+
+      const statusMessage = document.createElement('div')
+      statusMessage.classList.add('status')
+      form.appendChild(statusMessage)
+
+      const formData = new FormData(form)
+
+      try {
+        // const result = await postData('assets/server.php', formData)
+        // console.log('result -', result)
+        statusMessage.textContent = message.success
+      } catch {
+        console.log(error)
+        statusMessage.textContent = message.failure
+      }
+
+      clearInputs()
+      setTimeout(() => {
+        statusMessage.remove()
+      }, 5000)
+    })
+  })
+}
