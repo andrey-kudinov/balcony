aa5db83950eeb4da21eac91808d3e3820b372716
Andrey Kudinov <2499931@gmail.com>
2021-12-23 18:38:21 +0400

    add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,6 @@ import { modals, tabs } from './modules/';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals()
+  tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active')
+  tabs('.decoration_slider','.no_click', '.decoration_content > div > div', 'after_click')
 })
